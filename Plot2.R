bded6bfa06587aef2b4a501d79f9f01f5c70a6cf
Mariza <marizacantu@users.noreply.github.com>
2015-10-11 11:23:39 -0500

    Create Plot2.R

---
+++ Plot2.R
@@ -0,0 +1,21 @@
+##setting directory
+setwd("C:/Users/Mariza/Documents/Cursos/Especialización estadística/Exploratory Data Analysis")
+
+##variable holding the data base
+householdFile <- "C:/Users/Mariza/Documents/Cursos/Especialización estadística/Exploratory Data Analysis/data/household_power_consumption.txt"
+
+##having troubles reading de variable, so creating by other way
+householdfile <- read.table("C:/Users/Mariza/Documents/Cursos/Especialización estadística/Exploratory Data Analysis/data/household_power_consumption.txt")
+
+##begginning plotting instructions
+plotData <- read.table("C:/Users/Mariza/Documents/Cursos/Especialización estadística/Exploratory Data Analysis/data/household_power_consumption.txt", header=T, sep=";", na.strings="?")
+
+##changing time variables
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+
+##plot 2
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+
+##using Rstudio no need coding to have chart view
