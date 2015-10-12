356792d39a5751738968ac00a220522455c81ad4
Mariza <marizacantu@users.noreply.github.com>
2015-10-11 20:13:55 -0500

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,29 @@
+setwd("C:/Users/Mariza/Documents/Cursos/Especialización estadística/Exploratory Data Analysis")
+
+##variable holding the data base
+householdFile <- "C:/Users/Mariza/Documents/Cursos/Especialización estadística/Exploratory Data Analysis/data/household_power_consumption.txt"
+
+##having troubles reading de variable, so creating by other way
+householdfile <- read.table("C:/Users/Mariza/Documents/Cursos/Especialización estadística/Exploratory Data Analysis/data/household_power_consumption.txt")
+
+##begginning plotting instructions
+plotData <- read.table("C:/Users/Mariza/Documents/Cursos/Especialización estadística/Exploratory Data Analysis/data/household_power_consumption.txt", header=T, sep=";", na.strings="?")
+
+##changing time variables
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+
+## Generating Plot 4
+labels <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+columnlines <- c("black","red","blue")
+par(mfrow=c(2,2))
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="green", xlab="", ylab="Global Active Power")
+plot(finalData$SetTime, finalData$Voltage, type="l", col="orange", xlab="datetime", ylab="Voltage")
+plot(finalData$SetTime, finalData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(finalData$SetTime, finalData$Sub_metering_2, type="l", col="red")
+lines(finalData$SetTime, finalData$Sub_metering_3, type="l", col="blue")
+legend("topright", bty="n", legend=labels, lty=1, col=columnlines)
+plot(finalData$SetTime, finalData$Global_reactive_power, type="l", col="blue", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
